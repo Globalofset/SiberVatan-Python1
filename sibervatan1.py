478df7700ca4cf0de6e01ee68c0f47867d83cc2d
Globalofset <155827435+Globalofset@users.noreply.github.com>
2024-01-17 19:01:25 +0300

    Add files via upload

---
+++ sibervatan1.py
@@ -0,0 +1,36 @@
+'''
+sayi1=42
+sayi2=-17
+dogru_mu= True
+yanlıs_mı=False 
+ondalik1=3.14
+ondalik2=0.5
+sayi_float = float(sayi1)
+print("int to float",sayi_float)
+
+sayi_int = int(ondalik1)
+print("float to şnt",sayi_int)
+
+
+sayi_bool = int(dogru_mu)
+print("bool to int ",sayi_bool)
+print(type(sayi_bool))
+
+kullanici_ad=input("lutfen adınızı giriniz ")
+print("hoş geldin",kullanici_ad)
+
+
+
+sayi1=float(input("1.sayıyı giriniz"))
+sayi2=float(input("2.sayıyı giriniz"))
+toplam=(sayi2+sayi2)
+print("girilen sayıların toplamı",toplam)
+if (sayi1>sayi2):
+    print("1.sayı 2. sayıdan büyük")
+else:
+     print("2.sayı 1. sayıdan büyük")
+
+sayi3=input("1.sayıyı girin")
+sayi4=input("2.sayıyı girin")
+print(int(sayi3)+int(sayi4))
+'''
